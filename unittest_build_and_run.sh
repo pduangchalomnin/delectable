4bb08aaed3c8a88ff212b497536e1652d7d3d6a5
pduangchalomnin <Patipat@dhcp129.merunorth109.iit.edu>
2016-04-13 12:24:37 -0500

    fixed unittest sh

--- unittest_build_and_run.sh
+++ unittest_build_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd src
-javac -cp /usr/share/java/junit4-4.11.jar:. edu/iit/cs445/delectable/unitTest/*.java
+javac -cp ../WebContent/WEB-INF/lib/*:/usr/share/java/junit4-4.11.jar:. edu/iit/cs445/delectable/unitTest/*.java
 java -cp /usr/share/java/junit4-4.11.jar:. org.junit.runner.JUnitCore unitTest.TestSuit
 find . -name “*.class” -type f -delete

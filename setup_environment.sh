7a434ab6aed8dcbe06c2ed9ffd7d4a9e866a93e6
pduangchalomnin <Patipat@dhcp129.merunorth109.iit.edu>
2016-04-13 12:06:42 -0500

    fixed sh files

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install default-idk
-sudo apt-get install jUnit4
-sudo apt-get install tomcat7
-sudo service tomcat7 start
+sudo apt-get install -y default-jdk
+sudo apt-get install -y jUnit4
+sudo apt-get install -y tomcat7
+sudo service tomcat7 -y start

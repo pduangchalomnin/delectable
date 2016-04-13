fc225d72a5dc0958e23c59a2aaf7bb7eefc26fb1
pduangchalomnin <Patipat@dhcp129.merunorth109.iit.edu>
2016-04-13 12:11:06 -0500

    fixed sh files

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,12 +4,12 @@ cd src
 javac -cp ../WebContent/WEB-INF/lib/*:.  edu/iit/cs445/delectable/rest/*.java edu/iit/cs445/delectable/entity/*.java edu/iit/cs445/delectable/interactor/*.java;
 cd ..
 sudo mkdir WebContent/WEB-INF/classes
-cp -R src WebContent/WEB-INF/classes
+cp -R src/edu WebContent/WEB-INF/classes
 cd WebContent/WEB-INF/classes
 find . -name “*.java” -type f -delete
 cd ../../../src
 find . -name “*.class” -type f -delete
 cd ../WebContent
-jar -cvf edu.iit.cs445.delectable.war
+jar -cvf edu.iit.cs445.delectable.war *
 sudo cp edu.iit.cs445.delectable.war /var/lib/tomcat7/webapps
 sudo service tomcat7 restart

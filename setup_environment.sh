19608c542a86e61ce1ec75b32ae996b53ac1fa44
pduangchalomnin <Patipat@dhcp129.merunorth109.iit.edu>
2016-04-21 16:20:13 -0500

    update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install -y default-jdk
+sudo apt-get install openjdk-7-jdk
 sudo apt-get install -y jUnit4
 sudo apt-get install -y tomcat7
 sudo service tomcat7 start
